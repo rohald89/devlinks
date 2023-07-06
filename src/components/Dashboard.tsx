3089e52181a263007a0023a852f15b452f667321
Rohald van Merode <rohald_89@live.nl>
2023-07-06 22:06:47 +0200

    finished styling for Profile Details tab content

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -41,43 +41,59 @@ const Dashboard: FC<DashboardProps> = ({}) => {
           <p className="mt-2 text-body-md text-gray-500 ">
             Add your details to create a personal touch to your profile.
           </p>
-          <div className="mt-10 p-5 rounded-xl bg-background">
-            <p className="text-body-md mb-4 text-gray-500">Profile Picture</p>
-            <label
-              htmlFor="picture"
-              className="bg-primary-100 text-heading-sm text-primary-600 px-10 py-16 rounded-xl flex flex-col items-center gap-2 justify-center aspect-square max-w-[200px]"
-            >
-              <Icons.image />
-              + Upload Image
-              <Input type="file" id="picture" className="hidden" />
-            </label>
-            <p className="text-body-sm text-gray-500 mt-6">
-              Add your details to create a personal touch to your profile.
+          <div className="mt-10 p-5 rounded-xl bg-background md:flex md:items-center">
+            <p className="text-body-md mb-4 text-gray-500 md:basis-2/5">
+              Profile Picture
             </p>
+            <div className="basis-3/5 md:flex md:items-center">
+              <label
+                htmlFor="picture"
+                className="bg-primary-100 text-heading-sm text-primary-600 px-10 py-16 rounded-xl flex flex-col items-center gap-2 justify-center aspect-square md:min-w-[200px] w-[200px]"
+              >
+                <Icons.image />
+                + Upload Image
+                <Input type="file" id="picture" className="hidden" />
+              </label>
+              <p className="text-body-sm text-gray-500 mt-6 md:mt-0 md:ml-6">
+                Image must be below 1024x1024px. Use PNG or JPG format.
+              </p>
+            </div>
           </div>
 
           <div className="mt-6 p-5 rounded-xl bg-background">
-            <Label htmlFor="firstName">First name*</Label>
-            <Input
-              type="text"
-              id="firstName"
-              placeholder="Enter your first name"
-              className="mb-3"
-            />
-            <Label htmlFor="lastName">Last name*</Label>
-            <Input
-              type="text"
-              id="lastName"
-              placeholder="Enter your last name"
-              className="mb-3"
-            />
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className="mb-3"
-            />
+            <div className="md:flex items-center">
+              <Label htmlFor="firstName" className="md:basis-2/5">
+                First name*
+              </Label>
+              <Input
+                type="text"
+                id="firstName"
+                placeholder="Enter your first name"
+                className="mb-3 md:basis-3/5"
+              />
+            </div>
+            <div className="md:flex items-center">
+              <Label htmlFor="lastName" className="md:basis-2/5">
+                Last name*
+              </Label>
+              <Input
+                type="text"
+                id="lastName"
+                placeholder="Enter your last name"
+                className="mb-3 md:basis-3/5"
+              />
+            </div>
+            <div className="md:flex items-center">
+              <Label htmlFor="email" className="md:basis-2/5">
+                Email
+              </Label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className="mb-3 md:basis-3/5"
+              />
+            </div>
           </div>
         </div>
         <div className="p-4">
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -24,13 +24,13 @@ const getIconsFromName = (iconName: Icons) => {
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, icon, ...props }, ref) => {
     return (
-      <div className="relative">
+      <div className={cn('relative', className)}>
         <input
           type={type}
           className={cn(
             icon ? 'pl-9' : 'pl-3',
-            'flex h-10 w-full rounded-md text-gray-900 border border-gray-300 bg-white pr-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:shadow-active focus-visible:outline-none focus-visible:border-primary-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 file:border-0 file:bg-transparent file:text-sm file:font-medium ',
-            className
+            'flex h-10 w-full rounded-md text-gray-900 border border-gray-300 bg-white pr-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:shadow-active focus-visible:outline-none focus-visible:border-primary-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 file:border-0 file:bg-transparent file:text-sm file:font-medium '
+            // className
           )}
           ref={ref}
           {...props}
