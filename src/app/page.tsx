4b4bc4bb875fe7eb5cd883fcbab2254fdae4c9fb
Rohald van Merode <rohald_89@live.nl>
2023-07-06 11:10:56 +0200

    setup tailwind config for colors and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>My Homepage</h1>
-    </div>
-  );
+  return <></>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      'heading-md': [
+        '2rem',
+        {
+          lineHeight: '150%',
+          fontWeight: '700',
+        },
+      ],
+      'heading-sm': [
+        '1rem',
+        {
+          lineHeight: '150%',
+          fontWeight: '600',
+        },
+      ],
+      'body-md': [
+        '1rem',
+        {
+          lineHeight: '150%',
+        },
+      ],
+      'body-sm': [
+        '.75rem',
+        {
+          lineHeight: '150%',
+        },
+      ],
+    },
+    extend: {
+      colors: {
+        primary: {
+          600: 'hsl(var(--primary))',
+          300: '#BEADFF',
+          100: '#EFEBFF',
+        },
+        gray: {
+          900: '#333333',
+          500: '#737373',
+          300: 'D9D9D9',
+          100: '#FAFAFA',
+        },
+        error: 'FF3939',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
   },
   plugins: [],
 };
