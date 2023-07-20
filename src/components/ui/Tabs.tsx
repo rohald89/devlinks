17a9f5ca88bcadce6d25b00f6a864b7fc74c8e09
Rohald van Merode <rohald_89@live.nl>
2023-07-20 14:11:51 +0200

    finished dark styles for Tabs

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'text-gray-500 inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary-100 data-[state=active]:text-primary-600 data-[state=active]:shadow-sm',
+      'text-gray-500 inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary-100 data-[state=active]:text-primary-600 data-[state=active]:shadow-sm dark:data-[state=active]:bg-primary-600 dark:data-[state=active]:text-slate-100',
       className
     )}
     {...props}
