19556173a6808061df4a199ccae8a7f5ace17ab4
Rohald van Merode <rohald_89@live.nl>
2023-07-21 13:23:54 +0200

    debuggin auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,14 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    cookieName:
+      process.env.NODE_ENV === 'production'
+        ? '__Secure-next-auth.session-token'
+        : 'next-auth.session-token',
+  });
   console.log(token);
 
   if (!token) {
