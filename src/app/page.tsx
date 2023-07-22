b30310c0d70b431fbe11c04a8626407176abf891
Rohald van Merode <rohald_89@live.nl>
2023-07-22 14:42:31 +0200

    minor styling improvements throughout, finished styling Hero and added header to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,39 @@ import { LoginButton, LogoutButton } from '@/components/Auth';
 import ClientSession from '@/components/ClientSession';
 import { getAuthSession } from '@/lib/auth';
 
-import { Icons } from '@/components/Icons';
 import Hero from '@/components/Hero';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/Button';
+import { Icons } from '@/components/Icons';
 
 export default async function Home() {
   const session = await getAuthSession();
 
   return (
-    <>
-      <h1 className="text-2xl font-bold text-center md:text-heading-md">
-        One day, I will be a beautiful landing page 🚀
-      </h1>
+    <div className="relative bg-slate-900 md:pt-6">
+      <header className="fixed z-10 w-full md:w-11/12 md:left-1/2 md:-translate-x-1/2 bg-white dark:bg-slate-950 rounded-b-xl p-4 flex items-center justify-between md:rounded-xl">
+        {/* TODO find cleaner way for mobile / desktop differences */}
+        <span className="md:hidden">
+          <Icons.smallLogo />
+        </span>
+        <span className="hidden md:block">
+          <Icons.logo
+            width={136}
+            height={32}
+            className="text-gray-900 dark:text-slate-100"
+          />
+        </span>
+
+        <Link
+          href="/signin"
+          className={buttonVariants({
+            variant: 'outline',
+            className: 'px-4 md:px-7',
+          })}
+        >
+          <span className="">Login</span>
+        </Link>
+      </header>
 
       <Hero />
       <LoginButton />
@@ -22,6 +44,6 @@ export default async function Home() {
 
       <h2>Client Session</h2>
       <ClientSession />
-    </>
+    </div>
   );
 }
